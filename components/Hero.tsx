a24d460634c65f1b0ec8024bfa468fce5bc34715
AnisTT <trabelsianis046@gmail.com>
2024-10-18 22:36:56 +0100

    fourth build

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,14 +26,14 @@ const Hero = () => {
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
 
-      <div className="flex justify-center items-center md:gap-3 gap-6">
+      <div className="absolute top-4 right-4 flex justify-center items-center md:gap-3 gap-6">
         {socialMedia.map((info) => (
           <a key={info.id} target='blank' href={info.link}>
-            <div
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-            >
-              <img src={info.img} alt="icons" width={20} height={20} />
-            </div>
+        <div
+          className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+        >
+          <img src={info.img} alt="icons" width={20} height={20} />
+        </div>
           </a>
         ))}
       </div>
